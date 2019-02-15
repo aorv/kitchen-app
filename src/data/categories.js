70fbe7ad8088aa6218ea9c3cdcbc2f3d7e390d49
Andy Orvis <me@andyorvis.co.uk>
2019-02-15 11:32:25 +0000

    adding all sandwich data

--- src/data/categories.js
+++ src/data/categories.js
@@ -19,6 +19,38 @@ export const categories = [
       {
         name: "Roast ham, chedder & picalilli",
         price: prices.classics
+      },
+      {
+        name: "Kealey's roast beef with onion, mayo, mustard & horseradish",
+        price: prices.classics
+      },
+      {
+        name: "BLT",
+        price: prices.classics
+      },
+      {
+        name: "Praws with lemon mayo",
+        price: prices.classics
+      },
+      {
+        name: "Roast chicken & smoked bacon ceaser",
+        price: prices.classics
+      },
+      {
+        name: "Homemade coronation chicken & mango chutney",
+        price: prices.classics
+      },
+      {
+        name: "Roast chicken & smoked bacon club",
+        price: prices.classics
+      },
+      {
+        name: "Ham, cheese & homemade coleslaw",
+        price: prices.classics
+      },
+      {
+        name: "French brie & cranberry with bacon",
+        price: prices.classics
       }
     ]
   },
@@ -36,6 +68,30 @@ export const categories = [
       {
         name: "Bacon, sausage & black pudding",
         price: prices.hots
+      },
+      {
+        name: "Roast ham, chedder & chutney",
+        price: prices.hots
+      },
+      {
+        name: "Smoked bacon, brie & cranberry",
+        price: prices.hots
+      },
+      {
+        name: "Arthur Haigh Sausage, smoked chedder & onion marmalade",
+        price: prices.hots
+      },
+      {
+        name: "Mozzerlla, salami, pesto & sunblush tomato",
+        price: prices.hots
+      },
+      {
+        name: "Roast ham, chilli jam & mature chedder",
+        price: prices.hots
+      },
+      {
+        name: "Goats cheese, chorizo & tomato chutney",
+        price: prices.hots
       }
     ]
   },
@@ -51,7 +107,43 @@ export const categories = [
         price: prices.specials
       },
       {
-        name: "Roast beef, Yorkshire blue & onion marmalade",
+        name: "Roast beef, Yorkshire Blue & onion marmalade",
+        price: prices.specials
+      },
+      {
+        name: "Piri piri chicken & sweet chilli jam",
+        price: prices.specials
+      },
+      {
+        name: "Arthur Haigh Sausage, smoked chedder & mustard",
+        price: prices.specials
+      },
+      {
+        name: "Goats cheese, sunblush tomatoes & tomato chutney",
+        price: prices.specials
+      },
+      {
+        name: "Roast ham, cream cheese & sweet chilli jam",
+        price: prices.specials
+      },
+      {
+        name: "Homemade hummus, salami & sunblush tomatoes",
+        price: prices.specials
+      },
+      {
+        name: "Fresh salmon, cream cheese & mayo",
+        price: prices.specials
+      },
+      {
+        name: "Fresh salmon & prawns in lemon mayo",
+        price: prices.specials
+      },
+      {
+        name: "French brie & parma ham with apple & peach chutney",
+        price: prices.specials
+      },
+      {
+        name: "Mozzerlla, pesto, sunblush tomatoes & salami",
         price: prices.specials
       }
     ]
